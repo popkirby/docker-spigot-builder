909b75dc7a00cc87709121161ba20b7d5d435377
popkirby <popkirby@gmail.com>
2014-12-09 20:24:18 +0900

    fix typo

--- scripts/spigot-update.sh
+++ scripts/spigot-update.sh
@@ -3,5 +3,5 @@
 cd /tmp/spigot
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar
-mv -f Spigot/Spigot-Server/target/spigot-1.8-R0.1-snapshot.jar /minecraft_data/spigot.jar
+mv -f Spigot/Spigot-Server/target/spigot-1.8-R0.1-SNAPSHOT.jar /minecraft_data/spigot.jar
 
